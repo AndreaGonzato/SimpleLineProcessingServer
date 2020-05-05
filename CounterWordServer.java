4c20d50f1cceb87eb2e076d12212d3c78dcf67b2
Andrea Gonzato <gonzatoandrea98@gmail.com>
2020-05-05 09:43:37 +0200

    completed

--- CounterWordServer.java
+++ CounterWordServer.java
@@ -5,11 +5,19 @@ public class CounterWordServer extends SimpleLineProcessingServer {
   public CounterWordServer(int port, String quitCommand, OutputStream os) {
     super(port, quitCommand, os);
   }
-
+  /*
+    Hx code := char
+   \x2E = .
+   \x2C = ,
+   \x3B = ;
+   \x3A = :
+   \x3F = ?
+   \x21 = !
+   */
   @Override
   protected String process(String input) {
-    String trimString = input.trim().replaceAll("\\s+", " ");
-    String[] words = trimString.split("\\s|\\.|\\,");
+    String oneSpaceFromWordsLineString = input.trim().replaceAll("\\s+", " ");
+    String[] words = oneSpaceFromWordsLineString.split("\\s|\\t|\\x2E|\\x2C|\\x3B|\\x3A|\\x3F|\\x21");
     return String.valueOf(words.length);
   }
 }
